8c8b8d6ac2c0fe77a5f4389d24ba0524ce223b49
prowebstudio <prowebstudio@users.noreply.github.com>
2018-06-25 10:47:25 -0600

    Objects, properties, functions
    Understanding the relationship between objects properties and functions.

--- js/script.js
+++ js/script.js
@@ -5,8 +5,15 @@ it should hava a displayTodos method
 it should have an addTodos method
 it should have a changeTodo method
 it should have a deleteTodo method
+group a set of related data in an object
 */
-
+//OBJECTS HAVE PROPERTIES, name of property:
+var myComputer = {
+	operatingSystem: "mac",
+	screenSize: "15 inches",
+	purchaseYear: 2011
+	
+};
 //var todosList = {
 //	todos: ["item 1","item 2","item 3"] //When you setting the value of property you do not end the value with a semicolumn
 //	
@@ -18,5 +25,12 @@ var todoList = {
 	console.log("My Todos:",this.todos);
 	}
 
+};
+//Relationship between objects and functions
+var edgar = {
+	name:"Edgar", //this is property
+	sayName: function(){//no need to name the function you'll call it my the name of property
+		console.log("Hello"this.sayName);
+	}
 };
 
