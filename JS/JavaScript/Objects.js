3a864e10b16a2f397447b33eb08f98b9019c4146
StudioJava <38365682+StudioJava@users.noreply.github.com>
2018-07-11 11:46:38 -0600

    Accessing Object Properties and printing to the DOM

---
+++ JS/JavaScript/Objects.js
@@ -0,0 +1,13 @@
+var student = {
+    name: "Dave",
+    grades: [80,85,90,95],
+    age: 34,
+    country: "US",
+    registeredStudent: true
+};
+//Ways to access the name of property
+alert(student['name']);
+alert(student.name); //Using dot notation
+student.name = "Jon";
+student.state = "California"; //object a collection on variables
+
